fc2838442ec8f6bf50dd6fb22eb96ff33c3c1e94
kirshanth <krishanthkirsh@gmail.com>
2019-05-30 02:53:45 +0530

    DI first commit

---
+++ SampleDIApp/SampleDIApp/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SampleDIApp.DominClass;
+using SampleDIApp.Models;
+
+namespace SampleDIApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeService employeeService;
+
+        public string displayMessage(IEmployeeService _empserviceObj)
+        {
+            employeeService = _empserviceObj;
+            string Message = employeeService.DisplayEmployees();
+            return Message;
+        }
+
+        public IActionResult Index()
+        {
+            EmployeeService _empserviceObj = new EmployeeService();
+            ViewData["Message"] = displayMessage(_empserviceObj);
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SampleDIApp/SampleDIApp/DominClass/EmployeeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleDIApp.DominClass
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public string DisplayEmployees()
+        {
+            return "Dependency injection Example";
+        }
+    }
+}
